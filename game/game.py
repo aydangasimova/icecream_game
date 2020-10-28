03e42b12706052733ecac0a28d638ec184291668
Gasimova, Aydan <aydan.gasimova@kpn.com>
2020-10-29 00:38:09 +0100

    cleans up the game, it works

--- game/game.py
+++ game/game.py
@@ -2,12 +2,12 @@ import random
 from icecream_game.game.game_config import (STARTING_BUDGET,
                                             PURCHASE_PRICE_ICE,
                                             PURCHASE_PRICE_CONE,
-                                            BALANCE_THRESHOLD)
+                                            OPERATING_COST)
 
 
 def get_max_ice_cream_sales(temperature: float, price: float) -> float:
     """Formula for maximum sales of ice cream during a day"""
-    return 1000*temperature * (10-price)
+    return 1000 * temperature * (10 - price)
 
 
 def get_actual_ice_cream_sales(max_ice_cream_sales: float, stock_ice_cream: int, stock_cones: int) -> float:
@@ -36,19 +36,16 @@ def prepare_financial_report(game_round,
                              purchase_price_ice,
                              num_cones_bought,
                              purchase_price_cone,
-                             current_balance,
-                             balance_threshold,
+                             starting_money,
+                             operating_cost,
                              selling_price,
                              icecreams_sold):
-
-    starting_money = current_balance
-    operating_cost = balance_threshold
-    ice_expenses = purchase_price_ice*num_ice_bought
-    cone_expenses = purchase_price_cone*num_cones_bought
-    income = selling_price*icecreams_sold
+    ice_expenses = purchase_price_ice * num_ice_bought
+    cone_expenses = purchase_price_cone * num_cones_bought
+    income = selling_price * icecreams_sold
     total = starting_money - operating_cost - ice_expenses - cone_expenses + income
 
-    print("=============================================\n",
+    print("==============================================\n",
           "=============Finances Round", game_round, "===============\n",
           "=============================================\n",
           "Starting Money", starting_money, "\n",
@@ -56,46 +53,68 @@ def prepare_financial_report(game_round,
           "Ice Cream Expenses", ice_expenses, "\n",
           "Cone Expenses", cone_expenses, "\n",
           "Income", income, "\n",
+          "=============================================\n",
           "Total", total, "\n")
 
     return total
 
 
-def play_game(weather_today,
-              starting_balance,
+def play_game(starting_balance,
               purchase_price_ice,
               purchase_price_cone,
-              operating_cost,
-              max_ice_cream_sales) -> None:
+              operating_cost) -> None:
     """play the game"""
 
     game_round = 1
     current_balance = starting_balance
+    game_continues = "Y"
+    stock_ice_cream = 0
+    stock_cones = 0
 
-    while current_balance >= operating_cost:
-        print("The current weather today is", weather_today, "degrees celsius \n",
-              "Your current balance is ", current_balance)
+    while (current_balance >= operating_cost) & (game_continues == "Y"):
+            print("The current weather today is", get_weather(), "degrees celsius \n",
+                  "Your current balance is ", current_balance)
 
-        print("Stock up your Ice ($", purchase_price_ice, "), don't forget to keep some money for the cones (max:",
-              max_ice_cream_sales, ")")
-        num_ice_bought = input(int())
-        current_balance -= num_ice_bought*purchase_price_ice
+            print("Stock up your Ice ($", purchase_price_ice, "), don't forget to keep some money for the cones (max:",
+                  get_max_ice_cream_sales(get_weather(), purchase_price_ice), ")")
+            num_ice_bought = int(input())
 
-        print("Stock up your Ice Cones ($", purchase_price_cone,
-              "), don't forget to keep some money for the cones (max:",
-              max_ice_cream_sales, ")")
-        num_cones_bought = input(int())
-        current_balance -= num_cones_bought * purchase_price_cone
+            print("Stock up your Ice Cones ($", purchase_price_cone,
+                  "), don't forget to keep some money for the cones (max:",
+                  max_ice_cream_sales, ")")
+            num_cones_bought = int(input())
 
-        print("Set the selling price for your delicious ice creams $(x.xx)")
-        selling_price = input(float())
+            print("Set the selling price for your delicious ice creams $(x.xx)")
+            selling_price = float(input())
 
-        print("You sell", get_actual_ice_cream_sales(weather_today(), selling_price))
+            ice_creams_sold = get_actual_ice_cream_sales(get_weather(),
+                                                        stock_ice_cream + num_ice_bought,
+                                                        stock_cones + num_cones_bought)
+            print("You sell", ice_creams_sold)
 
-        game_round += 1
+            current_balance = prepare_financial_report(game_round,
+                                                       num_ice_bought,
+                                                       purchase_price_ice,
+                                                       num_cones_bought,
+                                                       purchase_price_cone,
+                                                       current_balance,
+                                                       operating_cost,
+                                                       selling_price,
+                                                       ice_creams_sold)
 
+
+            print("Do you want to continue (Y/N)?")
+            game_continues = input(str())
+            game_round += 1
+            stock_ice_cream = 0
+
+    print("Game over")
 
 
 if __name__ == "__main__":
     # init(sys.argv[1:])
     start_game()
+    play_game(STARTING_BUDGET,
+              PURCHASE_PRICE_ICE,
+              PURCHASE_PRICE_CONE,
+              OPERATING_COST)
